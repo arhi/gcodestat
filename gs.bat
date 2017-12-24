1a199729650272e2b48d124251626b59ae9be3f6
arhi <arhi.smece@gmail.com>
2017-12-24 02:59:41 +0100

    moved uploading to octoprint to gs.bat

--- gs.bat
+++ gs.bat
@@ -8,4 +8,5 @@ for %%a in (%1) do (
 set if=%filepath%%filename%%extension%
 set of=%filepath%%filename%_M117%extension%
 
-e:\Dev\eclipse-workspace\gcodestat\gcodestat.exe -d 0.02 -a 1000 --gcode="%if%" --output="%of%" | c:\Windows\System32\msg.exe %username%
+E:\Dev\eclipse-workspace\gcodestat\gcodestat.exe -d 0.02 -a 1000 --gcode="%if%" --output="%of%" | c:\Windows\System32\msg.exe %username%
+E:\bin\curl-7.50.3-win64-mingw\bin\curl.exe -H "X-Api-Key: XXXXXXYOURAPIKEYXXX" -F "select=false" -F "print=false" -F "file=@%of%" "http://octoprintip/api/files/local"
